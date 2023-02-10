f75a0fd76633474c841703987c3ca6f0ab20c25f
Sequoyah Walters <snwalters2@wisc.edu>
2023-02-10 01:10:17 -0600

    fixed landing bug

--- src/offb_node.cpp
+++ src/offb_node.cpp
@@ -1,15 +1,9 @@
 /**
  * @file offb_node.cpp
- * @brief Offboard control example node, written with MAVROS version 0.19.x, PX4 Pro Flight
- * Stack and tested in Gazebo SITL
+ * @brief Offboard control node for hardware flight using mavros and PX4
  */
 
 #include "offb_node.h"
-
-void stateCallback(const mavros_msgs::State::ConstPtr& msg);
-void cmdloopCallback(const ros::TimerEvent& event);
-void mavposeCallback(const geometry_msgs::PoseStamped &msg);
-void simInitCallback(const ros::TimerEvent& event);
 
 using namespace std;
 
@@ -18,42 +12,50 @@ int main(int argc, char **argv)
     ros::init(argc, argv, "offb_node");
     ros::NodeHandle nh;
 
+    OffbCtrl offbCtrl;
+    
+    // goal pose wrt home pose
+    offbCtrl.ref_offset.position.x = 1;
+    offbCtrl.ref_offset.position.y = 1;
+    offbCtrl.ref_offset.position.z = 1.5;
+    
     // Subscribers
-    ros::Subscriber state_sub = nh.subscribe
-            ("mavros/state", 1, stateCallback);
-    ros::Subscriber pose_sub = nh.subscribe
-            ("mavros/local_position/pose", 1, mavposeCallback, ros::TransportHints().tcpNoDelay());
+    offbCtrl.state_sub = nh.subscribe
+            ("mavros/state", 1, &OffbCtrl::stateCallback, &offbCtrl); // http://wiki.ros.org/roscpp_tutorials/Tutorials/UsingClassMethodsAsCallbacks
+    offbCtrl.pose_sub = nh.subscribe
+            ("mavros/local_position/pose", 1, &OffbCtrl::mavposeCallback, &offbCtrl, ros::TransportHints().tcpNoDelay());
     
     // Publishers
-    pos_pub = nh.advertise<geometry_msgs::PoseStamped>
+    offbCtrl.pos_pub = nh.advertise<geometry_msgs::PoseStamped>
             ("mavros/setpoint_position/local", 10);
     
     // Services        
-    arming_client = nh.serviceClient<mavros_msgs::CommandBool>
+    offbCtrl.arming_client = nh.serviceClient<mavros_msgs::CommandBool>
             ("mavros/cmd/arming");
-    set_mode_client = nh.serviceClient<mavros_msgs::SetMode>
+    offbCtrl.set_mode_client = nh.serviceClient<mavros_msgs::SetMode>
             ("mavros/set_mode");
     
     // Timers
-    cmdloop_timer = nh.createTimer(ros::Duration(0.01), cmdloopCallback); // send commands at 100 HZ
-    simInit_timer = nh.createTimer(ros::Duration(1), simInitCallback); // send commands at 1 HZ
+    bool oneshot = false;
+    bool autostart = false;
+    offbCtrl.cmdloop_timer = nh.createTimer(ros::Duration(0.01), &OffbCtrl::cmdloopCallback, &offbCtrl, oneshot, autostart); // send commands at 100 HZ
+    offbCtrl.sim_timer = nh.createTimer(ros::Duration(1), &OffbCtrl::simCallback, &offbCtrl, oneshot, autostart); // send commands at 1 HZ
             
     // Parameters
     // TODO: Implement the sim_enable parameter along with launch file
-    //nh_private_.param<bool>("enable_sim", sim_enable, true);
-    sim_enable = true;
+    //nh_private_.param<bool>("enable_sim", offbCtrl.sim_enable, true);
+    offbCtrl.sim_enable = true;
     
     // wait for FCU connection
     ros::Rate FCU_connect_rate(5.0);
-    while(ros::ok() && !current_state.connected){
+    while(ros::ok() && !offbCtrl.current_state.connected){
         ros::spinOnce();
         FCU_connect_rate.sleep();
     }
-
-    // goal pose wrt home pose
-    ref_offset.position.x = 0;
-    ref_offset.position.y = 0;
-    ref_offset.position.z = 1.5;
+    
+    // Start command loop and simInit
+    offbCtrl.cmdloop_timer.start();
+    offbCtrl.sim_timer.start();
 
     ros::spin();
     return 0;
@@ -62,54 +64,98 @@ int main(int argc, char **argv)
 // ---------- Functions ----------- //
 
 // Pass along mav state info
-void stateCallback(const mavros_msgs::State::ConstPtr& msg){
+void OffbCtrl::stateCallback(const mavros_msgs::State::ConstPtr& msg){
     current_state = *msg;
 }
 
-void cmdloopCallback(const ros::TimerEvent& event) {
+// Publish flight commands according to current flight state
+void OffbCtrl::cmdloopCallback(const ros::TimerEvent& event) {
   switch (flight_state) {
     case WAITING_FOR_HOME_POSE: {
       waitForPose(&received_home_pose, "Waiting for home pose...");
       ROS_INFO("Got pose! Drone Ready to be armed.");
-      flight_state = MISSION_EXECUTION;
+      flight_state = TAKEOFF;
       break;
     }
-
-    case MISSION_EXECUTION: {
+    case TAKEOFF: {
+      // hover over home position
+      geometry_msgs::PoseStamped takeoff_msg;
+      takeoff_msg.header.stamp = ros::Time::now();
+      takeoff_msg.pose = home_pose;
+      takeoff_msg.pose.position.z += 1.0;
+      double x_diff = takeoff_msg.pose.position.x - curr_pose.position.x;
+      double y_diff = takeoff_msg.pose.position.y - curr_pose.position.y;
+      double z_diff = takeoff_msg.pose.position.z - curr_pose.position.z;
+      double pos_error = sqrt(pow(x_diff,2.) + pow(y_diff,2.) + pow(z_diff,2.)); // in meters
+      if (pos_error > 0.1){
+        pos_pub.publish(takeoff_msg);
+      } else {
+        flight_state = MISSION;
+        ros::spinOnce();
+      }
+      break;
+    }
+    case MISSION: {
       double x_diff = ref_pose.position.x - curr_pose.position.x;
       double y_diff = ref_pose.position.y - curr_pose.position.y;
       double z_diff = ref_pose.position.z - curr_pose.position.z;
       double pos_error = sqrt(pow(x_diff,2.) + pow(y_diff,2.) + pow(z_diff,2.)); // in meters
-        
-      if (pos_error > 0.01) {
+      if (ros::Time::now() - print_request > ros::Duration(2.0)){
+        cout << "x diff: " << x_diff << endl;
+        cout << "y diff: " << y_diff << endl;
+        cout << "z diff: " << z_diff << endl;
+        cout << "total error: " << pos_error << endl << endl;
+        print_request = ros::Time::now();
+      }
+      if (pos_error > 0.06) {
         ref_msg.header.stamp = ros::Time::now();
         ref_msg.pose = ref_pose;
         pos_pub.publish(ref_msg);
       } else {
-        flight_state = LANDING;
+        flight_state = RETURNING;
+        pos_pub.publish(ref_msg);
       }
       break;
     }
-
-    case LANDING: {
-      geometry_msgs::PoseStamped landingmsg;
-      landingmsg.header.stamp = ros::Time::now();
-      landingmsg.pose = home_pose;
-      landingmsg.pose.position.z = landingmsg.pose.position.z + 1.0;
-      pos_pub.publish(landingmsg);
-      flight_state = LANDED;
-      ros::spinOnce();
+    case RETURNING: {
+      // hover over home position
+      geometry_msgs::PoseStamped return_msg;
+      return_msg.header.stamp = ros::Time::now();
+      return_msg.pose = home_pose;
+      return_msg.pose.position.z = home_pose.position.z + 0.5;
+      double x_diff = return_msg.pose.position.x - curr_pose.position.x;
+      double y_diff = return_msg.pose.position.y - curr_pose.position.y;
+      double z_diff = return_msg.pose.position.z - curr_pose.position.z;
+      double pos_error = sqrt(pow(x_diff,2.) + pow(y_diff,2.) + pow(z_diff,2.)); // in meters
+      if (pos_error > 0.1){
+        pos_pub.publish(return_msg);
+      } else {
+        flight_state = LANDING;
+        ros::spinOnce();
+      }
       break;
     }
-    case LANDED: {
-      ROS_INFO("Landed. Please set to position control and disarm.");
-      cmdloop_timer.stop();
+    case LANDING: {
+      // auto land
+      offb_set_mode.request.custom_mode = "AUTO.LAND";
+      if (current_state.mode != "AUTO.LAND" && (ros::Time::now() - last_request > ros::Duration(2.0))) {
+        if (set_mode_client.call(offb_set_mode) && offb_set_mode.response.mode_sent) {
+          ROS_INFO("AUTO.LAND enabled");
+          ROS_INFO("Once landed, switch to position mode and disarm.");
+          cmdloop_timer.stop();
+        }
+        last_request = ros::Time::now();
+      }
       break;
+    }
+    default: {
+      ROS_INFO("Error: Flight State Not Defined");
     }
   }
 }
 
-void mavposeCallback(const geometry_msgs::PoseStamped &msg) {
+// Pass along mav pose data. Sets reference pose upon first call.
+void OffbCtrl::mavposeCallback(const geometry_msgs::PoseStamped &msg) {
   if (!received_home_pose) {
     received_home_pose = true;
     home_pose = msg.pose;
@@ -120,31 +166,45 @@ void mavposeCallback(const geometry_msgs::PoseStamped &msg) {
     temp.position.y += ref_offset.position.y;
     temp.position.z += ref_offset.position.z;
     ref_pose = temp;
+    ROS_INFO_STREAM("Reference offset initialized to: " << ref_offset);
   }
   curr_pose = msg.pose;
 }
 
-void simInitCallback(const ros::TimerEvent &event) {
+// Enable OFFBOARD mode and ARM, used for simulation
+void OffbCtrl::simCallback(const ros::TimerEvent &event) {
     if (sim_enable) {
-        ROS_INFO("sim init");
-        // Enable OFFBoard mode and arm automatically
-        // ONLY run if the vehicle is simulated
-        arm_cmd.request.value = true;
-        offb_set_mode.request.custom_mode = "OFFBOARD";
-        if (current_state.mode != "OFFBOARD" && (ros::Time::now() - last_request > ros::Duration(5.0))) {
-            if (set_mode_client.call(offb_set_mode) && offb_set_mode.response.mode_sent) {
-            ROS_INFO("Offboard enabled");
+        switch(flight_state) {
+            case LANDING: {
+                // try to disarm
+                arm_cmd.request.value = false;
+                if (current_state.armed && (ros::Time::now() - last_request > ros::Duration(2.0))) {
+                    if (arming_client.call(arm_cmd) && arm_cmd.response.success) {
+                        ROS_INFO("Vehicle disarmed");
+                        sim_timer.stop();
+                    }
+                    last_request = ros::Time::now();
+                }
             }
-            last_request = ros::Time::now();
-        } else {
-            if (!current_state.armed && (ros::Time::now() - last_request > ros::Duration(5.0))) {
-            if (arming_client.call(arm_cmd) && arm_cmd.response.success) {
-                ROS_INFO("Vehicle armed");
-            }
-            last_request = ros::Time::now();
+            default: {
+                arm_cmd.request.value = true;
+                offb_set_mode.request.custom_mode = "OFFBOARD";
+                if (current_state.mode != "OFFBOARD" && (ros::Time::now() - last_request > ros::Duration(2.0))) {
+                    if (set_mode_client.call(offb_set_mode) && offb_set_mode.response.mode_sent) {
+                        ROS_INFO("Offboard enabled");
+                    }
+                    last_request = ros::Time::now();
+                } else {
+                    if (!current_state.armed && (ros::Time::now() - last_request > ros::Duration(2.0))) {
+                        if (arming_client.call(arm_cmd) && arm_cmd.response.success) {
+                            ROS_INFO("Vehicle armed");
+                        }
+                        last_request = ros::Time::now();
+                    }
+                }
             }
         }
-    } else {
-        simInit_timer.stop();
+    } else { // not using sim
+        sim_timer.stop();
     }
 }
--- src/offb_node.h
+++ src/offb_node.h
@@ -13,38 +13,56 @@
 
 using namespace std;
 
-enum FlightState { WAITING_FOR_HOME_POSE, MISSION_EXECUTION, LANDING, LANDED } flight_state;
+class OffbCtrl {
+    public:
+        OffbCtrl(){
+            flight_state = WAITING_FOR_HOME_POSE;
+        }
+        
+        bool sim_enable;
+        geometry_msgs::Pose ref_offset;
+        mavros_msgs::State current_state;
 
-mavros_msgs::State current_state;
-mavros_msgs::CommandBool arm_cmd;
-mavros_msgs::SetMode offb_set_mode;
-geometry_msgs::Pose home_pose;
-geometry_msgs::PoseStamped ref_msg;
-geometry_msgs::Pose ref_pose;
-geometry_msgs::Pose ref_offset;
-geometry_msgs::Pose curr_pose;
+        ros::Subscriber state_sub;
+        ros::Subscriber pose_sub;
+        ros::Publisher pos_pub;
+        ros::ServiceClient arming_client;
+        ros::ServiceClient set_mode_client;
+        ros::Timer cmdloop_timer;
+        ros::Timer sim_timer;
+        
+        void stateCallback(const mavros_msgs::State::ConstPtr& msg);    // Pass along mav state info
+        void cmdloopCallback(const ros::TimerEvent& event);             // Publish flight commands according to current flight state
+        void mavposeCallback(const geometry_msgs::PoseStamped &msg);    // Pass along mav pose data. Sets reference pose upon first call.
+        void simCallback(const ros::TimerEvent& event);                 // Enable OFFBOARD mode and ARM, used for simulation
 
-ros::Time last_request;
+    private:
+        enum FlightState { WAITING_FOR_HOME_POSE, TAKEOFF, MISSION, RETURNING, LANDING } flight_state;
+        
+        mavros_msgs::CommandBool arm_cmd;
+        mavros_msgs::SetMode offb_set_mode;
 
-ros::Publisher pos_pub;
+        geometry_msgs::Pose home_pose;
+        geometry_msgs::PoseStamped ref_msg;
+        geometry_msgs::Pose ref_pose;
+        geometry_msgs::Pose curr_pose;
 
-ros::Timer cmdloop_timer;
-ros::Timer simInit_timer;
+        ros::Time last_request;
+        ros::Time land_time;
+        ros::Time print_request;
+        bool received_home_pose;
 
-ros::ServiceClient arming_client;
-ros::ServiceClient set_mode_client;
-
-bool sim_enable;
-bool received_home_pose;
-
-template <class T>
-void waitForPose(const T *first_pose, const std::string &msg, double hz = 2.0) {
-    ros::Rate wait_rate(hz);
-    ROS_INFO_STREAM(msg);
-    while (ros::ok() && !(*first_pose)) {
-        ros::spinOnce();
-        wait_rate.sleep();
-    }
+        template <class T>
+        void waitForPose(const T *first_pose, const std::string &msg, double hz = 2.0) {
+            ros::Rate wait_rate(hz);
+            ROS_INFO_STREAM(msg);
+            while (ros::ok() && !(*first_pose)) {
+                ros::spinOnce();
+                wait_rate.sleep();
+            }
+        };
 };
+
+
 
 #endif
